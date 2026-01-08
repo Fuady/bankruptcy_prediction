f27f7ab14b02ed3bf6a3f6adfa7c45e21c8b8c50
Fuady <a.m.fuady@gmail.com>
2026-01-08 10:59:46 +0700

    train

--- train.py
+++ train.py
@@ -23,11 +23,6 @@ from sklearn.impute import SimpleImputer
 # =========================
 MODEL_PATH = "models/fraud_model.pkl"
 
-col= []
-for a in range(1,65):
-    col.append('x'+str(a))
-col.append('y')
-
 year_1 = arff.loadarff("data/1year.arff")
 df_year_1 = pd.DataFrame(year_1[0])
 
@@ -35,6 +30,10 @@ df_year_1 = pd.DataFrame(year_1[0])
 # =========================
 # 2. Preprocessing
 # =========================
+col= []
+for a in range(1,65):
+    col.append('x'+str(a))
+col.append('y')
 
 df_year_1.columns = col
 df_year_1['y'] = df_year_1['y'].str.decode('utf-8')
