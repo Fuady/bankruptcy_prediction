48745b86b12b8ed5a737aef2bd79c3671a522d8b
Fuady <a.m.fuady@gmail.com>
2026-01-12 13:40:26 +0700

    add_predict

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 # -----------------------------
 # Load trained model
 # -----------------------------
-MODEL_PATH = "models/fraud_model.pkl"
+MODEL_PATH = "models/bankruptcy_model.pkl"
 
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
